71166a60e32db2e5ed7c1f77c5285c0011c3ce2f
aluczakzero <adrianl2710@wp.pl>
2022-05-14 14:02:41 +0200

    add json data for calendar

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from pprint import pp
+
 from custom_calendar import Calendar
 from data.utils import EventGenerator
-from pprint import pprint as pp
+from event import Event
 
 data = EventGenerator()
-data.generate(500)
-c = Calendar(data.events)
+# data.save('./data.json')
+events_data = data.load('./data.json')
 
+
+events = [Event(**event) for event in events_data]
+pp(events_data)
+c = Calendar(events)
 result = c.get_events_by_date('01-01-2023 00:00')
 print(result, len(result))
-c.remove_event('17-05-2022 9:00')
-# pp(c)
+# c.remove_event('17-05-2022 9:00')
